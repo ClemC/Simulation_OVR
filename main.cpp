ae101d8d3b62cc160b6d34a3cc67aece9ccf6cb2
a <couchet.arnaud@gmail.com>
2015-01-13 18:36:22 +0100

    Readme.md updated with the installation tutorial.

--- main.cpp
+++ main.cpp
@@ -16,11 +16,11 @@ int main(int argc, char** argv)
                 ("oculus,o", "Oculus mode")
                 ("fullscreen,f", "Fullscreen mode")
                 ("texture,t", po::value<std::string>()->default_value("Textures/photorealistic/photorealistic_marble/granit01.jpg"), "Set the texture used on the cubes")
-                ("number,n", po::value<unsigned long>()->default_value(8192), "Set the number of objects seen") // useless with file.
-                ("size,s", po::value<int>()->default_value(8192), "Set the size of the data cube. Must be a power of 2") // Up to 8192 with 20fps !
+                ("number,n", po::value<unsigned long>()->default_value(1024), "Set the number of objects seen") // useless with file.
+                ("size,s", po::value<int>()->default_value(1024), "Set the size of the data cube. Must be a power of 2") // Up to 8192 with 20fps !
                 ("octantSize", po::value<int>()->default_value(8), "Set the size of an octant. Must be a power of 2")
                 ("octantDrawnCount,d", po::value<int>()->default_value(8), "Set the number of octant drawn count. 1 to only draw the octant the camera is currently in, 2 to draw the immediate neighbors, ...")
-                ("read,r", po::value<std::string>()->default_value("./File/star_00012"), "Set the file to read.") // set "./File/star.txt" or "" here.
+                ("read,r", po::value<std::string>()->default_value("./File/star_00020"), "Set the file to read.") // set "./File/star.txt" or "" here.
                 ("randomPercentage,a", po::value<int>()->default_value(100), "Set the percentage of points to display with the random algorithm. If 100, do not use random method.")
                 ("clusteringPercentage,c", po::value<int>()->default_value(100), "Set the percentage of points to display with the hierarchical clustering algorithm. If 100, do not use clustering method.")
                 ("isMultiThread,m", po::value<bool>()->default_value(false), "Enable multithreading.")
@@ -37,8 +37,8 @@ int main(int argc, char** argv)
             return 0;
         }
         Scene scene("Immersive-3D visualization for astronomical data", WINDOW_WIDTH, WINDOW_HEIGHT,
-                    vm.count("oculus"),
-                    vm.count("fullscreen"),
+                    1,
+                    1,
                     vm["texture"].as<std::string>(),
                 vm["number"].as<unsigned long>(),
                 vm["size"].as<int>(),
